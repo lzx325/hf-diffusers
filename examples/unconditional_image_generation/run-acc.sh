7e82ef4cd6b60730f9ce24997e445e44f49d1ead
Zhongxiao Li <liz0f@gpu502-11.ibex.kaust.edu.sa>
2023-07-18 18:04:40 +0300

    run-acc

--- examples/unconditional_image_generation/run-acc.sh
+++ examples/unconditional_image_generation/run-acc.sh
@@ -3,7 +3,6 @@
 	conda deactivate
 	conda activate diffusers
 
-<<<<<<< HEAD
 	out_dir="ddpm-ema-flowers-64-acc"
 	rm -rf "$out_dir"
 	option="$1"
@@ -66,38 +65,3 @@
 	esac
 
 }
-=======
-    out_dir="ddpm-ema-flowers-64-2gpu"
-    rm -rf "$out_dir"
-
-    # python -u -m accelerate.commands.launch \
-    #     --num_processes=2 \
-    #     train_unconditional.py \
-    #     --dataset_name="huggan/flowers-102-categories" \
-    #     --resolution=64 \
-    #     --center_crop --random_flip \
-    #     --output_dir="$out_dir" \
-    #     --train_batch_size=32 \
-    #     --checkpointing_steps=100 \
-    #     --num_epochs=2 \
-    #     --gradient_accumulation_steps=1 \
-    #     --use_ema \
-    #     --learning_rate=1e-4 \
-    #     --lr_warmup_steps=500 \
-    #     --mixed_precision=no
-    
-    python train_unconditional.py \
-        --dataset_name="huggan/flowers-102-categories" \
-        --resolution=64 \
-        --center_crop --random_flip \
-        --output_dir="$out_dir" \
-        --train_batch_size=32 \
-        --checkpointing_steps=100 \
-        --num_epochs=2 \
-        --gradient_accumulation_steps=1 \
-        --use_ema \
-        --learning_rate=1e-4 \
-        --lr_warmup_steps=500 \
-        --mixed_precision=no
-}
->>>>>>> 3934cdcb7b63035f6f84003d7dfbb98e3fad5f90
