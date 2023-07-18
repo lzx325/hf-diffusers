3934cdcb7b63035f6f84003d7dfbb98e3fad5f90
lzx325 <lzx325@outlook.com>
2023-07-18 17:13:18 +0300

    second

--- examples/unconditional_image_generation/run-acc.sh
+++ examples/unconditional_image_generation/run-acc.sh
@@ -5,15 +5,31 @@
 
     out_dir="ddpm-ema-flowers-64-2gpu"
     rm -rf "$out_dir"
-    python -u -m accelerate.commands.launch \
-        --num_processes=2 \
-        train_unconditional.py \
+
+    # python -u -m accelerate.commands.launch \
+    #     --num_processes=2 \
+    #     train_unconditional.py \
+    #     --dataset_name="huggan/flowers-102-categories" \
+    #     --resolution=64 \
+    #     --center_crop --random_flip \
+    #     --output_dir="$out_dir" \
+    #     --train_batch_size=32 \
+    #     --checkpointing_steps=100 \
+    #     --num_epochs=2 \
+    #     --gradient_accumulation_steps=1 \
+    #     --use_ema \
+    #     --learning_rate=1e-4 \
+    #     --lr_warmup_steps=500 \
+    #     --mixed_precision=no
+    
+    python train_unconditional.py \
         --dataset_name="huggan/flowers-102-categories" \
         --resolution=64 \
         --center_crop --random_flip \
         --output_dir="$out_dir" \
         --train_batch_size=32 \
-        --num_epochs=50 \
+        --checkpointing_steps=100 \
+        --num_epochs=2 \
         --gradient_accumulation_steps=1 \
         --use_ema \
         --learning_rate=1e-4 \
